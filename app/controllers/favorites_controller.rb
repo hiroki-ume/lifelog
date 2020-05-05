7f83a20b10b34450d7dac2e5bb32aa7db6744c30
hiroki-ume <h.ume1219bknb@gmail.com>
2020-05-05 17:16:50 +0900

    いいねの一覧表示機能の追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,11 @@
 class FavoritesController < ApplicationController
+  before_action :set_post
+
+  def index
+    @favorites = @post.favorites.all
+  end
+
   def create
-    @post = Post.find(params[:post_id])
     @favorite = current_user.favorites.new(post_id: @post.id)
     @favorite.save
     flash[:success] = "いいねしました"
@@ -8,10 +13,14 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     @favorite = current_user.favorites.find_by(post_id: @post.id)
     @favorite.destroy
     flash[:notice] = "いいね を取り消しました"
     redirect_back(fallback_location: root_path)
   end
+
+  private
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
 end
