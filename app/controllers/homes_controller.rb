6225c50e22473c6ff17dec682c05bc40ba8b3705
hiroki-ume <h.ume1219bknb@gmail.com>
2020-05-09 15:38:52 +0900

    未ログイン時にトップページを表示しようとすると生じるエラーの解消

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def top
     @user = current_user
-    @posts = @user.posts.all.order(id: "DESC") 
+    @posts = @user.posts.all.order(id: "DESC") if @user_signed_in
   end
 
   def about
