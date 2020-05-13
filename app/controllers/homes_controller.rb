443eca55589c45e6e3eca0a4bc9c48e1195edd38
hiroki-ume <h.ume1219bknb@gmail.com>
2020-05-13 14:46:43 +0900

    全体的なレイアウトの変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,6 @@
 class HomesController < ApplicationController
   def top
-    @user = current_user
-    @posts = @user.posts.all.order(id: "DESC") if @user_signed_in
+    @posts = current_user.posts.all.order(id: "DESC") if user_signed_in?
   end
 
   def about
